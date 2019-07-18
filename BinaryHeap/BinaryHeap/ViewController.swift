563252735205ed12cfacfdb6294418c2dc623c6a
Egon Fiedler <egonfiedler@Egons-MacBook-Air.local>
2019-07-18 04:24:40 -0700

    This is a submodule made for an example of a BinaryHeap could be made and tested on Swift5

---
+++ BinaryHeap/BinaryHeap/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BinaryHeap
+//
+//  Created by Egon Fiedler on 7/18/19.
+//  Copyright © 2019 Egon Fiedler. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ BinaryHeap/BinaryHeapTests/BinaryHeapTests.swift
@@ -0,0 +1,34 @@
+//
+//  BinaryHeapTests.swift
+//  BinaryHeapTests
+//
+//  Created by Egon Fiedler on 7/18/19.
+//  Copyright © 2019 Egon Fiedler. All rights reserved.
+//
+
+import XCTest
+@testable import BinaryHeap
+
+class BinaryHeapTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
